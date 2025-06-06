8cdff314b9b91c1a96a36c09d4745e6289959c1b
Joshua-Alfredo-Flores-Deleon <20240228@ricaldone.edu.sv>
2025-06-06 10:32:28 -0600

    Se termino el apartado de agregar

--- js/script.js
+++ js/script.js
@@ -74,8 +74,29 @@ document.getElementById("frmAgregar").addEventListener("submit", async e => {
 
     //Llamar a la API para enviar el registro
     const respuesta = await fetch(API_URL, {
-        method: "POST",
-        
+        method: "POST", //Tipo de solicitud
+        headers: {'Content-Type':'application/json'}, //Tipo de dato enviado
+        body: JSON.stringify({nombre, apellido, correo}) //Datos enviados
     });
 
+    //Verificar si la API responde que los datos fueron enviados corectamente
+    if(respuesta.ok){
+        alert("El registro fue agregado correctamente");
+
+        //Limpiar el formulario
+        document.getElementById("frmAgregar").reset();
+
+        //Cerrar el modal (dialog)
+        modal.close();
+
+        //Recargar la tabla
+        ObtenerIntegrantes();
+    }
+    else{
+        //En caso de que la API devuelva un codigo diferente  a 200-299
+        alert("El registro no pudo ser agregado");
+    }
+
 });
+
+//Eliminar un nuevo integrante
